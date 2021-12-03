e1c0ad5c550c4ed8914ca387619009fa89c2cfd0
Freedisch <freeproduc@gmail.com>
2021-12-03 20:50:00 +0100

    2.8

--- DealCom/urls.py
+++ DealCom/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('',views.index,name="acceuil"),
+    path('',views.homepage,name="acceuil"),
     path('produit/',views.product,name="produit"),
     path('about/',views.about,name="about"),
 ]
--- DealCom/views.py
+++ DealCom/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse, request
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
-def index(request):
-    return render(request,"DealCom/Home.html")
+def homepage(request):
+    products=Product.objects.all()
+
+    data={
+        'products':products
+    }
+    return render(request,"DealCom/Home.html",data)
 
 def about(request):
     return render(request,"DealCom/AboutUs.html")
